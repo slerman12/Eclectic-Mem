7f64cbd1c1d8b284e5e1d38c499c07913033e303
sam <slerman12@gmail.com>
2020-12-05 20:15:58 -0500

    Cleaner

--- Agent.py
+++ Agent.py
@@ -5,22 +5,34 @@ from math import inf
 
 
 class Memory:
-    def __init__(self, N=inf, memories=None):
+    def __init__(self, N=inf):
         self.N = N
+        self.n = 0
+        self.memories = {}
 
-        if memories is None:
-            self.n = 0
-            self.memories = {}
+    def add(self, memories):
+        if not isinstance(memories, list):
+            memories = [memories]
+
+        for m in memories:
+            if self.n == self.N:
+                self.delete_LRA()
+
+            self.memories[m.id] = m
+
+    def retrieve(self, ids):
+        if isinstance(ids, list):
+            return [self.memories[ID] for ID in ids]
         else:
-            self.memories = copy(memories)
+            return self. memories[ids]
 
 
 class MemoryUnit:
     def __init__(self, concept, reward, action):
         self.id = secrets.token_bytes()
-        self.c = concept
-        self.r = reward
-        self.a = action
+        self.concept = concept
+        self.reward = reward
+        self.action = action
         self.futures = Memory()
         self.pasts = Memory()
 
@@ -39,28 +51,26 @@ class Agent:
         new
         c_t = self.embed(o_t)
 
+        self.M_t_minus_1 = self.M_t
         margin = 0.5
-        self.M_t = Memory(memories={m: self.M_t_minus_1.memories[m] for m in self.M_t_minus_1.futures()
-                                    if self.delta(c_t, self.M_t_minus_1.memories[m].c()) > margin})
+        self.M_t = Memory()
+        self.M_t.add([m for m in self.M_t_minus_1.get_futures() if self.delta(c_t, m.concept) > margin])
 
         if len(self.M_t.memories) == 0:
+            n
             self.M_t = self.traverse(self.M_t_minus_1,)
 
         self.M_t = self.update(self.M_t, c_t)
 
         a_t = self.policy(c_t, self.M_t)
 
-        if len(self.M_t.memories) == 0 or a_t not in self.M_t.a():
-            self.store(c_t, r_t, a_t)
+        if self.M_t.n == 0 or a_t not in self.M_t.get_a():
+            nn
+            m = MemoryUnit(c_t, r_t, a_t)
+            self.Memory.add(m)
+            self.M_t.add(m)
 
-
-
-    def store(self, c, r, a):
-        if self.n == self.N:
-            self.delete_LRA()
-        m = MemoryUnit(c, r, a)
-        self.Memory.add(m)
-        self.M_t.add(m)
+        if
 
     def learn(self):
 # todo contrastive learning
