eb52566c87f71859e755351630e7bcf4cbb26dec
samlerman <slerman@ur.rochester.edu>
2021-04-08 16:07:32 -0400

    debugging q dimensionality

--- Eclectic-Mem/Memory.py
+++ Eclectic-Mem/Memory.py
@@ -35,7 +35,6 @@ class Memory(Module):
             kwargs["t"] = torch.tensor([self.time] * batch_size).unsqueeze(dim=1)
         self.time += 0.001
         for key in kwargs:
-            print(key, kwargs[key].shape)
             assert kwargs[key].shape[0] == batch_size  # batches only
             # get current memories for key or set default
             memory = getattr(self, key, torch.empty([self.N] + list(kwargs[key].shape)[1:]))
@@ -66,7 +65,6 @@ class Memory(Module):
         expected_q = None
         if weigh_q:
             # B x k x 1, B * k -> B
-            print(self.q.shape, self.q[indices].shape, self.q[indices].squeeze(-1).shape, deltas.shape)
             expected_q = (self.q[indices].squeeze(-1) * deltas).sum(-1)
         assert deltas.shape[0] == c.shape[0] and deltas.shape[1] == k  # todo debugging check, can delete
 
