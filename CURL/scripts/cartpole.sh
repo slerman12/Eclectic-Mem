a4c91dad8628694e551b26919e446d95fe765b50
JIng Bi <jbi5@ur.rochester.edu>
2021-04-08 22:01:48 -0400

    changed script

--- CURL/scripts/cartpole.sh
+++ CURL/scripts/cartpole.sh
@@ -4,5 +4,5 @@ CUDA_VISIBLE_DEVICES=3 python train.py \
     --encoder_type pixel \
     --action_repeat 8 \
     --pre_transform_image_size 100 --image_size 84 \
-    --agent em_sac --frame_stack 3 \
+    --agent curl_sac --frame_stack 3 \
     --seed -1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000
