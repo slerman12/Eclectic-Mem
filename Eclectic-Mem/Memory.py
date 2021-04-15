1b309ce160047aef5e0040c81b277d119bd34e6d
samlerman <slerman@ur.rochester.edu>
2021-04-15 18:35:56 -0400

    Debugging memory param

--- Eclectic-Mem/Memory.py
+++ Eclectic-Mem/Memory.py
@@ -74,8 +74,8 @@ class Memory(Module):
             # memory = getattr(self, key, Parameter(torch.empty([self.N] + list(kwargs[key].shape)[1:])).to(self.device))
             memory = getattr(self, key, torch.empty([self.N] + list(kwargs[key].shape)[1:]).to(self.device))
             # append new memories to them
-            memory[batch_size:].data = memory[:-batch_size].data.to(self.device)
-            memory[batch_size:].data = kwargs[key].data.to(self.device)
+            memory[batch_size:].data.copy(memory[:-batch_size].data.to(self.device))
+            memory[batch_size:].data.copy(kwargs[key].data.to(self.device))
             # memory = torch.cat((kwargs[key].to(self.device), memory[:-batch_size])).to(self.device)
             # TODO parameter memory
             # new_memory = Parameter(torch.cat((kwargs[key].to(self.device), memory[:-batch_size]))).to(self.device)
