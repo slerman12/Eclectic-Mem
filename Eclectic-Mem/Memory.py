589a23196d342180fc75cde58a6ad36217352056
samlerman <slerman@ur.rochester.edu>
2021-04-15 18:06:31 -0400

    Debugging memory param

--- Eclectic-Mem/Memory.py
+++ Eclectic-Mem/Memory.py
@@ -76,12 +76,13 @@ class Memory(Module):
             print(key in self.__dict__)
             # memory = getattr(self, key, torch.empty([self.N] + list(kwargs[key].shape)[1:]).to(self.device))
             # append new memories to them
-            new_memory = torch.cat((kwargs[key].to(self.device), memory[:-batch_size])).to(self.device)
+            # new_memory = torch.cat((kwargs[key].to(self.device), memory[:-batch_size])).to(self.device)
             # TODO parameter memory
-            # new_memory = Parameter(torch.cat((kwargs[key].to(self.device), memory[:-batch_size]))).to(self.device)
+            new_memory = Parameter(torch.cat((kwargs[key].to(self.device), memory[:-batch_size]))).to(self.device)
             # with torch.no_grad():
             #     param.copy_(torch.randn(10, 10))
             setattr(self, key, new_memory)
+            print(key in self.__dict__)
             self.memory[key] = self.__dict__[key]
         assert self.c.shape[0] >= self.n  # todo debugging check, can delete
 
