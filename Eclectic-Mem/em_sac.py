43b860a0029b4e5699af48da67763c3502a05ecb
samlerman <slerman@ur.rochester.edu>
2021-04-15 20:48:07 -0400

    Debugging memory param

--- Eclectic-Mem/em_sac.py
+++ Eclectic-Mem/em_sac.py
@@ -481,12 +481,7 @@ class EclecticMemCurlSacAgent(object):
             L.log('train_critic/loss_q2', critic_loss_Q2, step)
             L.log('train_critic/loss_q3', critic_loss_Q3, step)
 
-        # Optimize the critic
-        self.critic_optimizer.zero_grad()
-        critic_loss.backward()
-        self.critic_optimizer.step()
-
-        # Update Eclectic-Mem
+        # Update EclecticMem
         if self.critic.memory is not None:
             # note: could also add c_prime and c_prime_next
             # mem = {"c": c, "c_next": c_next, "r": reward, "q": target_Q, "a": action, "d": not_done, "step": step}
@@ -495,6 +490,11 @@ class EclecticMemCurlSacAgent(object):
                    # "c_next": c_next,
                    "r": reward, "q": target_Q.detach(), "a": action.detach(), "d": not_done}
             self.critic.memory.add(**mem)
+
+        # Optimize the critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
 
         self.critic.log(L, step)
 
