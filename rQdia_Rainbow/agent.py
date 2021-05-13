c0bc9edbafc136c8e17da88813447ae2a3556f04
samlerman <slerman@ur.rochester.edu>
2021-05-13 11:13:28 -0400

    random shift change

--- rQdia_Rainbow/agent.py
+++ rQdia_Rainbow/agent.py
@@ -18,8 +18,8 @@ import torch.nn as nn
 from model import DQN
 import torch.nn.functional as F
 
-# random_shift = nn.Sequential(aug.RandomCrop((80, 80)), nn.ReplicationPad2d(4), aug.RandomCrop((84, 84)))
-random_shift = nn.Sequential(nn.ReplicationPad2d(4), aug.RandomCrop((84, 84)))
+random_shift = nn.Sequential(aug.RandomCrop((80, 80)), nn.ReplicationPad2d(4), aug.RandomCrop((84, 84)))
+# random_shift = nn.Sequential(nn.ReplicationPad2d(4), aug.RandomCrop((84, 84)))
 aug = random_shift
 
 class Agent():
