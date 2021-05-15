1a9e9498421db86867580c896ed99b3477182bd6
samlerman <slerman@ur.rochester.edu>
2021-05-15 15:18:56 -0400

    sbatch slurm sweep cpus shell

--- rQdia_Rainbow/atari_cpu_sweep.sh
+++ rQdia_Rainbow/atari_cpu_sweep.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 module load python3/3.8.3
+seed=1
 for game in "ms_pacman" "pong"
 do
-	python3 sbatch.py --cpu --params "--game $game"
+	python3 sbatch.py --cpu --name $game$seed --params "--game $game --seed $seed"
 	sleep 2
 done
