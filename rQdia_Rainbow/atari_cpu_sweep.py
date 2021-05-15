b579edcdc0408f7545d4e66466740128931d224a
samlerman <slerman@ur.rochester.edu>
2021-05-15 15:09:00 -0400

    sbatch slurm sweep cpus

--- rQdia_Rainbow/atari_cpu_sweep.py
+++ rQdia_Rainbow/atari_cpu_sweep.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-
+os.system("module load python3/3.8.3")
 for game in ["ms_pacman", "pong"]:
     os.system("python3 sbatch.py --cpu --params '--game {}'".format(game))
     time.sleep(2)
