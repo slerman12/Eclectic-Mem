bae962f9de293bbfb9a50c58668341eb5f5ce7f0
samlerman <slerman@ur.rochester.edu>
2021-05-15 16:50:44 -0400

    updated script action repeats

--- rQdia_DrQ/scripts/finger.sh
+++ rQdia_DrQ/scripts/finger.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=3 python train.py --expname rQdia-dqr-finger-spin \
     --domain_name finger \
     --task_name spin \
     --encoder_type pixel \
-    --action_repeat 8 \
+    --action_repeat 2 \
     --pre_transform_image_size 84 --image_size 84 \
     --agent curl_sac --frame_stack 3 \
-    --seed -1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000
+    --seed -1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 500000
--- rQdia_SAC/scripts/walker.sh
+++ rQdia_SAC/scripts/walker.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=2 python train.py --expname rQdia-curl-walker-walk \
+CUDA_VISIBLE_DEVICES=2 python train.py --expname rQdia-sac-walker-walk \
     --domain_name walker \
     --task_name walk \
     --encoder_type pixel \
-    --action_repeat 8 \
+    --action_repeat 2 \
     --pre_transform_image_size 84 --image_size 84 \
     --agent curl_sac --frame_stack 3 \
-    --seed -1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000
+    --seed -1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 500000
