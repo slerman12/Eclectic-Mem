f981e5e87ada37b32e1cfcaa06c67729d6e38dfd
samlerman <slerman@ur.rochester.edu>
2021-05-16 12:19:00 -0400

    cpus bluehive

--- rQdia_Rainbow/atari_cpu_sweep.sh
+++ rQdia_Rainbow/atari_cpu_sweep.sh
@@ -4,6 +4,6 @@ seed=1
 #for game in "alien" "amidar" "assault" "asterix" "bank_heist" "battle_zone" "boxing" "breakout" "chopper_command" "crazy_climber" "demon_attack" "freeway" "frostbite" "gopher" "hero" "jamesbond" "kangaroo" "krull" "kung_fu_master" "ms_pacman" "pong" "private_eye" "qbert" "road_runner" "seaquest" "up_n_down"
 for game in "ms_pacman" "pong"
 do
-	python3 sbatch.py --cpu --name $game$seed --params "--game $game --seed $seed"
+	python3 sbatch.py --cpu --name $game$seed --params "--game $game --seed $seed --expname $game$seed"
 	sleep 2
 done
