d01a71d4943a9a8e8c68384771dd9bcbe21992cd
samlerman <slerman@ur.rochester.edu>
2021-05-17 13:29:13 -0400

    curl scripys

--- CURL/scripts/walker.sh
+++ CURL/scripts/walker.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=2 python train.py \
+CUDA_VISIBLE_DEVICES=2 python train.py --expname curl-walker-walk-${1:--1} \
     --domain_name walker \
     --task_name walk \
     --encoder_type pixel \
-    --action_repeat 8 \
-    --pre_transform_image_size 100 --image_size 84 \
+    --action_repeat 2 \
+    --pre_transform_image_size 84 --image_size 84 \
     --agent curl_sac --frame_stack 3 \
-    --seed -1 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 1000000
+    --seed ${1:--1} --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 500000
