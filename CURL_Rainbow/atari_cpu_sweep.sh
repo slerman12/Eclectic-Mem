434cfd17bdf8bd16fba079c53f21b2dc24f4a759
samlerman <slerman@ur.rochester.edu>
2021-05-19 14:44:16 -0400

    curl rainbow bluehive

--- CURL_Rainbow/atari_cpu_sweep.sh
+++ CURL_Rainbow/atari_cpu_sweep.sh
@@ -4,6 +4,6 @@ seed=1
 for game in "alien" "amidar" "assault" "asterix" "bank_heist" "battle_zone" "boxing" "breakout" "chopper_command" "crazy_climber" "demon_attack" "freeway" "frostbite" "gopher" "hero" "jamesbond" "kangaroo" "krull" "kung_fu_master" "ms_pacman" "pong" "private_eye" "qbert" "road_runner" "seaquest" "up_n_down"
 #for game in "breakout"
 do
-	python3 sbatch.py --cpu --name $game$seed --params "--game $game --seed $seed --expname curl$game$seed"
+	python3 sbatch.py --cpu --name curl$game$seed --params "--game $game --seed $seed --expname curl$game$seed"
 	sleep 2
 done
