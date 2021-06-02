1c371ddf5bc72b01d5fb6dda37721cba2ecc176e
samlerman <slerman@ur.rochester.edu>
2021-06-01 23:11:33 -0400

    curl rainbow

--- CURL_Rainbow/atari_cpu_sweep.sh
+++ CURL_Rainbow/atari_cpu_sweep.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 module load python3/3.8.3
 seed=1
-#for game in "alien" "amidar" "assault" "asterix" "bank_heist" "battle_zone" "boxing" "breakout" "chopper_command" "crazy_climber" "demon_attack" "freeway" "frostbite" "gopher" "hero" "jamesbond" "kangaroo" "krull" "kung_fu_master" "ms_pacman" "pong" "private_eye" "qbert" "road_runner" "seaquest" "up_n_down"
-for game in "breakout" "jamesbond" "krull"
+for game in "alien" "amidar" "assault" "asterix" "bank_heist" "battle_zone" "boxing" "breakout" "chopper_command" "crazy_climber" "demon_attack" "freeway" "frostbite" "gopher" "hero" "jamesbond" "kangaroo" "krull" "kung_fu_master" "ms_pacman" "pong" "private_eye" "qbert" "road_runner" "seaquest" "up_n_down"
+#for game in "breakout" "jamesbond" "krull"
 #for game in "chopper_command" "asterix" "battle_zone" "bank_heist" "amidar" "assault" "alien"
 do
 	python3 sbatch.py --cpu --name curlre$game$seed --params "--game $game --seed $seed --expname curlr$game$seed"
